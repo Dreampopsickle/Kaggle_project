f80ff6cc0265126469aa1bd4f0531cd855e62611
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2024-04-20 14:13:25 -0400

    read data

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+//Install Dependencies
 const { exec } = require("child_process");
+const fs = require("fs");
+const csv = require("csv-parser");
+/*----------------------------------------*/
 
-exec(
-  "kaggle datasets download -d yakhyojon/tiktok -p ./ --unzip",
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
+//To download kaggle data set
+// exec(
+//   "kaggle datasets download -d yakhyojon/tiktok -p ./ --unzip",
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
+
+const results = [];
+
+fs.createReadStream("./data/tiktok_dataset.csv")
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    console.log(results);
+  });
